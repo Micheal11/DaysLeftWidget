90740f7ed67cf5d3c5b6a86ff91a0c013b4d1c93
Mutesasira Albert <katxalbert@gmail.com>
2017-08-10 05:45:32 +0300

    Remove table

--- src/DaysLeft/widget/DaysLeft.ts
+++ src/DaysLeft/widget/DaysLeft.ts
@@ -8,19 +8,20 @@ import * as dom from "dojo/dom";
 import "./ui/DaysLeft.css";
 
 class DaysLeft extends WidgetBase {
-    DateInserted: any;
-    NameOfEvent: string;
+    dateInserted: string;
+    eventName: string;
+    width: number;
+    height: number;
     private contextObject: mendix.lib.MxObject;
     private insertedEvent: string;
-    private insertedDate: any;
     private nextDate: Date;
-    private mendixDateGot: Date;
+    private dateFromMendix: Date;
     private currentDate: Date;
-    private tableRow: any;
 
     postCreate() {
         this.customize();
     }
+
     update(object: mendix.lib.MxObject, callback?: () => void) {
         this.contextObject = object;
         this.updateRendering();
@@ -28,65 +29,88 @@ class DaysLeft extends WidgetBase {
             callback();
         }
     }
-    resize(box: any) {
-        logger.debug(this.id + ".resize");
-    }
-    htmlTable() {
-        domConstruct.empty(this.domNode);
-        const leftDays = domConstruct.create("div", {
-            class: "days-left-widget"
-        }, this.domNode);
-        const raw1 = domConstruct.create("table", {
-           id: "setColor",
-            // tslint:disable-next-line:max-line-length
-            innerHTML: `<tr>${this.computeDays() }</tr><span><br><tr>${this.insertedEvent}</span></tr>`
-        }, leftDays);
-        if (this.computeDays() < 0) {
-            // tslint:disable-next-line:max-line-length
-            dom.byId("setColor").innerHTML = `<tr><td class = "set-to-cyan">${this.computeDays() * -1 + " " + "Days Since"}</td></tr><br><tr><td>${this.insertedEvent}<td></tr>`;
-        } else if (this.computeDays() <= 50) {
-            // tslint:disable-next-line:max-line-length
-            dom.byId("setColor").innerHTML = `<tr><td class = "set-to-red">${this.computeDays() + " " + "Days to"}</td></tr><br><tr><td>${this.insertedEvent}<td></tr>`;
-        } else if (this.computeDays() <= 100) {
-            // tslint:disable-next-line:max-line-length
-            dom.byId("setColor").innerHTML = `<tr><td class = "set-to-orange">${this.computeDays() + " " + "Days to"}</td></tr><br><tr><td>${this.insertedEvent}<td></tr>`;
+    updateRendering() {
+        if (this.contextObject) {
+            this.insertedEvent = this.contextObject.get(this.eventName).toString();
+            const parseDate = Number(this.contextObject.get(this.dateInserted));
+            this.nextDate = new Date(parseDate);
+            this.createDisplay();
+            dojoStyle.set(this.domNode, "display", "block");
         } else {
-            // tslint:disable-next-line:max-line-length
-            dom.byId("setColor").innerHTML = `<tr><td class = "set-to-crystalClear">${this.computeDays() + " " + "Days to"}</td></tr><br><br><tr><td>${this.insertedEvent}<td></tr>`;
+            dojoStyle.set(this.domNode, "display", "none");
+        }
+        dom.byId("mainContainer").setAttribute("style",
+            "width:" + this.width+ "px;" + "height:" + this.height + "px;")
+        dom.byId("mainContainer").setAttribute("style",
+    }
+
+    computeDays(): number {
+        this.dateFromMendix = this.nextDate;
+        this.currentDate = new Date();
+        return (this.dateDaysBetween(this.currentDate, this.dateFromMendix));
+    }
+
+    createDisplay() {
+        domConstruct.empty(this.domNode);
+        const leftDays = domConstruct.create("div", { 
+            id: "mainContainer",
+            innerHtml: `<span>
+                            ${this.computeDays()}
+                        </span><br/>
+                        <span>
+                            'Days to'
+                        </span><br/>
+                        <span>
+                            ${this.insertedEvent}
+                        </span><br/>`
+        }, this.domNode);
+
+        domConstruct.create("div", {
+            class: "widgetDaysLeft",
+            id: "setColor"
+        }, this.domNode);
+
+        if (this.computeDays() < 0) {
+            this.setColour("setColor", "Days Since", "widgetDaysLeftSetToCyan");
+        } else if (this.computeDays() < 15) {
+            this.setColour("setColor", "Days To", "widgetDaysLeftSetToRed");
+        } else if (this.computeDays() === 15) {
+            this.setColour("setColor", "Days To", "widgetDaysLeftSetToOrange");
+        } else {
+            this.setColour("setColor", "Days To", "widgetDaysLeftSetToGreen ");
         }
     }
+    private setColour(id: string, condition: string, className: string) {
+        dom.byId(id).innerHTML = `<div class = "${className}">
+                                    ${this.computeDays()}
+                                  </div><br/>
+                                  <div>
+                                    ${condition}
+                                  </div><br/>
+                                  <div>
+                                    ${this.insertedEvent}
+                                  <div>`;
+    }
+
     private customize() {
         domConstruct.create("div", {
-            class: "days-left-widget",
+            class: "widgetDaysLeft",
             id: "dayswidget"
         }, this.domNode);
     }
-    public computeDays(): number {
-        this.mendixDateGot = this.nextDate;
-        this.currentDate = new Date();
-        return (this.DatedaysBetween(this.currentDate, this.mendixDateGot));
-    }
-    private DatedaysBetween(date1: Date, date2: Date): number {
+
+    private dateDaysBetween(date1: Date, date2: Date): number {
         const oneDay = 1000 * 60 * 60 * 24;
         const date1Microsec = date1.getTime();
         const date2Microsec = date2.getTime();
         const differenceInMicrosec = date2Microsec - date1Microsec;
         return Math.ceil(differenceInMicrosec / oneDay);
     }
-    updateRendering() {
-        if (this.contextObject) {
-            this.insertedEvent = this.contextObject.get(this.NameOfEvent).toString();
-            this.insertedDate = this.contextObject.get(this.DateInserted);
-            const parseDate = Number(this.insertedDate);
-            this.nextDate = new Date(parseDate);
-            this.htmlTable();
-            dojoStyle.set(this.domNode, "display", "block");
-        } else {
-            dojoStyle.set(this.domNode, "display", "none");
-        }
-    }
+
 }
-dojoDeclare("DaysLeft.widget.DaysLeft", [ WidgetBase ], function(Source: any) {
+
+// tslint:disable-next-line:only-arrow-functions
+dojoDeclare("DaysLeft.widget.DaysLeft", [WidgetBase], function (Source: any) {
     const result: any = {};
     for (const i in Source.prototype) {
         if (i !== "constructor" && Source.prototype.hasOwnProperty(i)) {
